c71c824e541c54a0509455a7f83d48a37632028f
Angel VI <a.v.iliev13@gmail>
2022-12-21 17:16:55 +0200

    wip: piece positioning

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
+	"image/draw"
 	"image/png"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // 32-bit bitmask to determine the color of squares (all 1's represent black)
@@ -30,7 +35,28 @@ const (
 	imgWidth   = pxSize
 	imgHeight  = pxSize
 	boardSize  = 8
-	squareSize = pxSize / 8
+	squareSize = pxSize / boardSize
+)
+
+func load(filePath string) (*image.NRGBA, error) {
+	imgFile, err := os.Open(filePath)
+	defer imgFile.Close()
+	if err != nil {
+		return nil, fmt.Errorf("Cannot read file: %v", err)
+	}
+
+	img, _, err := image.Decode(imgFile)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot decode file: %v", err)
+	}
+	return img.(*image.NRGBA), nil
+}
+
+const (
+	piecesImg = "ChessPiecesArray.png"
+	pieceSize = 60
+	// NOTE: this assumes that square size is bigger than piece size
+	piecePadding = (squareSize - pieceSize) / 2
 )
 
 func main() {
@@ -38,6 +64,10 @@ func main() {
 	lowRight := image.Point{imgWidth, imgHeight}
 
 	img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+	pieces, err := load(piecesImg)
+	if err != nil {
+		log.Fatalf("couldn't load image %s: %v", piecesImg, err)
+	}
 
 	// Colors are defined by Red, Green, Blue, Alpha uint8 values.
 	// cyan := color.RGBA{100, 200, 200, 0xff}
@@ -53,8 +83,97 @@ func main() {
 			img.Set(x, y, IdxColor(sqIdx))
 		}
 	}
+	pieceRect, err := pieceRect("n", "b")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sqRect, err := notationRect("a1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(sqRect)
+	draw.Draw(img, sqRect, pieces.SubImage(pieceRect), pieceRect.Min, draw.Over)
 
 	// Encode as PNG.
 	f, _ := os.Create("image.png")
 	png.Encode(f, img)
+}
+
+const (
+	pieces = "qkrnbp"
+	colors = "bw"
+)
+
+func pieceRect(piece, color string) (image.Rectangle, error) {
+	piece = strings.ToLower(piece)
+	if piece == "" {
+		// treat empty string as pawn
+		piece = "p"
+	}
+
+	if len(piece) > 1 || !strings.Contains(pieces, piece) {
+		return image.Rectangle{}, fmt.Errorf("invalid piece: %s", strconv.Quote(piece))
+	}
+
+	pieceIdx := strings.Index(pieces, piece)
+
+	color = strings.ToLower(color)
+	if len(color) > 1 || !strings.Contains(colors, color) {
+		return image.Rectangle{}, fmt.Errorf("invalid color: %s", strconv.Quote(color))
+	}
+
+	colorIdx := strings.Index(colors, color)
+
+	rect := image.Rect(
+		pieceIdx*pieceSize,
+		colorIdx*pieceSize,
+		(pieceIdx+1)*pieceSize,
+		(colorIdx+1)*pieceSize,
+	)
+	return rect, nil
+}
+
+var (
+	Files = "abcdefgh"
+	Ranks = "12345678"
+)
+
+// notationRect returns a rectangle with corresponding location on the board
+// from square notation ("a1", "d3").
+func notationRect(notation string) (image.Rectangle, error) {
+	if len(notation) != 2 {
+		return image.Rectangle{}, fmt.Errorf(
+			"Wrong square notation format %s, expected format \"b7\"",
+			notation,
+		)
+	}
+
+	notation = strings.ToLower(notation)
+
+	fileStr := string(notation[0])
+	rankStr := string(notation[1])
+
+	if !strings.Contains(Ranks, rankStr) || !strings.Contains(Files, fileStr) {
+		return image.Rectangle{}, fmt.Errorf(
+			"Wrong square notation %s, rank/file not found",
+			notation,
+		)
+	}
+
+	file := strings.Index(Files, fileStr)
+
+	// a1 shuold be bottom of board but because we are drawing from top left
+	// then we have to make sure that a1 corresponding to index 0, 7 (instead of 0, 0)
+	rank, _ := strconv.Atoi(rankStr)
+	rank = boardSize - rank
+	log.Println(file, rank)
+
+	rect := image.Rect(
+		(file*squareSize)+piecePadding,
+		(rank*squareSize)+piecePadding,
+		(file+1)*squareSize,
+		(rank+1)*pieceSize,
+	)
+	return rect, nil
 }
